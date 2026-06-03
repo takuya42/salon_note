16e0f18c1d2e506e9a010da39500a3201199e4ce
takuya42 <116898264+takuya42@users.noreply.github.com>
2026-06-03 14:52:51 +0900

    Update web home shop list

---
+++ lib/web/providers/web_home_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/web_shop.dart';
+import 'web_shop_provider.dart';
+
+final webHomeShopsProvider = StreamProvider.autoDispose<List<WebShop>>((ref) {
+  return ref.watch(webShopServiceProvider).watchLatestShops(limit: 50);
+});
